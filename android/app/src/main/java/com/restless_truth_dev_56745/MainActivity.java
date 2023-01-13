f66eff60ec38da0f2dccade810472b48ec879f99
Crowdbotics <engineering@crowdbotics.com>
2023-01-13 22:02:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/restless_truth_dev_56745/MainActivity.java
@@ -0,0 +1,15 @@
+package com.restless_truth_dev_56745;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "restless_truth_dev_56745";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-truth-dev-56745",
+  url: "https://restless-truth-dev-56745.botics.co",
+  api: "https://restless-truth-dev-56745.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
